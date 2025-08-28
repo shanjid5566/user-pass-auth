9b0479f7aa6a4a5847256837e646592b444e0731
Shanjid Ahmed Rafi <shanjidahmed66@gmail.com>
2025-08-28 18:07:01 +0600

    Registation done

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,16 +1,18 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../../firebase.init";
 import { useState } from "react";
+import {FaRegEyeSlash, FaEye } from "react-icons/fa";
 
 const Register = () => {
   const [errorMsg, setErrorMsg] = useState("");
   const [successMsg, setSuccessMsg] = useState("");
+  const [showPass, setShowPass] = useState(false)
   const handleRegistation = (event) => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
     setErrorMsg("");
-    setSuccessMsg("")
+    setSuccessMsg("");
     const regex =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (!regex.test(password)) {
@@ -23,7 +25,7 @@ const Register = () => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log(userCredential.user);
-        setSuccessMsg("Registation Successful")
+        setSuccessMsg("Registation Successful");
       })
       .catch((error) => {
         setErrorMsg(error.message);
@@ -42,24 +44,23 @@ const Register = () => {
             placeholder="Email"
           />
           <label className="label">Password</label>
-          <input
-            type="password"
-            className="input"
-            name="password"
-            placeholder="Password"
-          />
+          <div className="relative">
+            <input
+              type={showPass ? "text" : "password"}
+              className="input"
+              name="password"
+              placeholder="Password"
+            />
+            <button onClick={()=>setShowPass(!showPass)} className="absolute top-4 right-7">
+                {showPass ? <FaRegEyeSlash /> : <FaEye />}
+            </button>
+          </div>
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
           <button className="btn btn-neutral mt-4">Login</button>
-          {errorMsg 
-                && 
-                <p className="text-red-500">{errorMsg}</p>
-            }
-            {successMsg 
-                && 
-                <p className="text-green-500">{successMsg}</p>
-            }
+          {errorMsg && <p className="text-red-500">{errorMsg}</p>}
+          {successMsg && <p className="text-green-500">{successMsg}</p>}
         </form>
       </div>
     </div>
